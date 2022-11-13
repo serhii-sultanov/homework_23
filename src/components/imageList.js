8620ae219ee310f816904389bd6b868e0768871d
serhii-sultanov <sshsultanov@gmail.com>
2022-11-13 14:14:01 +0200

    renamed component and state, replaced styles to index.css

--- src/components/imageList.js
+++ src/components/imageList.js
@@ -2,20 +2,20 @@ import { useState, useEffect } from "react";
 
 const ImageList = () => {
   const [images, setImages] = useState([]);
-  const [shown, setShown] = useState(1);
+  const [page, setPage] = useState(1);
   const [isLoading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetch(`https://picsum.photos/v2/list?page=${shown}&limit=10`)
+    fetch(`https://picsum.photos/v2/list?page=${page}&limit=10`)
       .then((response) => response.json())
       .then((photos) => {
         setImages([...photos, ...photos]);
         setLoading(false);
       });
-  }, [shown]);
+  }, [page]);
 
   const handleClick = () => {
-    setShown(shown + 1);
+    setPage((prevState) => prevState + 1);
   };
 
   if (isLoading) {
@@ -24,38 +24,17 @@ const ImageList = () => {
 
   return (
     <>
-      <h1 style={{ fontSize: "48px", textAlign: "center" }}>Image Gallery</h1>
-      <div
-        style={{
-          padding: "20px",
-          display: "flex",
-          flexWrap: "wrap",
-          justifyContent: "center",
-        }}
-      >
+      <h1 className="heading" style={{}}>
+        Image Gallery
+      </h1>
+      <div className="container">
         {images.map(({ id, download_url, author }) => (
-          <div key={id} style={{ width: "500px", margin: "20px" }}>
-            <img
-              src={download_url}
-              alt={author}
-              style={{ width: "100%" }}
-            ></img>
+          <div key={id} className="image-box">
+            <img className="image" src={download_url} alt={author}></img>
           </div>
         ))}
       </div>
-      <button
-        onClick={handleClick}
-        style={{
-          margin: "0 auto 30px",
-          display: "block",
-          padding: "15px 20px",
-          backgroundColor: "#24a0ed",
-          borderRadius: "20px",
-          fontSize: "24px",
-          color: "white",
-          border: "none",
-        }}
-      >
+      <button onClick={handleClick} className="btn">
         Show More
       </button>
     </>
